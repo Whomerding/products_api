c8f496d264fb282f76f1ff2aca3d9cd924fed9c6
Whomerding <wendy.homerding@gmail.com>
2023-01-31 12:27:42 -0500

    filled out logic for updating and deleting products from the DB in views.py + tested creating a product in postman

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
     if request.method == 'GET':
         products = Product.objects.all ()
@@ -16,3 +16,18 @@ def products_list(request):
         serializer.is_valid (raise_exception=True)
         serializer.save ()
         return Response (serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view (['GET', 'PUT', 'DELETE'])
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET': 
+        serializer = ProductSerializer(product)
+        return Response (serializer.data)
+    elif request.method == 'PUT':  
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response (status=status.HTTP_204_NO_CONTENT)
